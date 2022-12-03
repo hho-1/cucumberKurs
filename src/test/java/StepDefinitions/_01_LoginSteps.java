daad9efbf3922e913a3206043a1d1050349f0965
Habip Hakkioglu <h.hakkioglu@icloud.com>
2022-12-03 21:31:26 +0100

    Login and Country Add Functions

---
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -0,0 +1,51 @@
+package StepDefinitions;
+
+import Pages.DialogContent;
+import Utilities.GenelWebDriver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+import static Utilities.GenelWebDriver.driver;
+
+public class _01_LoginSteps {
+
+    DialogContent dc = new DialogContent();
+
+    @Given("Navigate to basqar")
+    public void navigateToBasqar() {
+
+        GenelWebDriver.getDriver().get("https://demo.mersys.io/");
+        GenelWebDriver.getDriver().manage().window().maximize();
+    }
+
+    @When("Enter username and password and click on login button")
+    public void enterUsernameAndPasswordAndClickOnLoginButton() {
+
+        //dc.username.sendKeys("richfield.edu");
+        //dc.password.sendKeys("Richfield2020!");
+        //dc.loginButton.click();
+
+        dc.findAndSendKeys("username", "richfield.edu");
+        dc.findAndSendKeys("password", "Richfield2020!");
+        dc.findAndClick("loginButton");
+
+    }
+
+    @Then("User should be logged in successfully")
+    public void userShouldBeLoggedInSuccessfully() {
+
+        //dc.waitUntilVisible(dc.dashboard);
+        //Assert.assertTrue(dc.dashboard.getText().contains("Dashboard"));
+
+        dc.checkIfContainsText("dashboard", "dashboard");
+
+    }
+}
